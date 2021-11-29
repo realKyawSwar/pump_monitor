f24e55c06563e750b44b9ea6ab94e11e45241240
realKyawSwar <kyaw.s.thein@gmail.com>
2021-11-29 17:39:53 +0800

    clean up
    output:
    [bytearray(b'@00RE004542000151*\\r'), bytearray(b'@00RE004543000150*\\r'), bytearray(b'@00RE004544000157*\\r'), bytearray(b'@00RE004545000156*\\r'), bytearray(b'@00RE004546000155*\\r'), bytearray(b'@00RE004550000152*\\r'), bytearray(b'@00RE004552000150*\\r'), bytearray(b'@00RE004553000151*\\r'), bytearray(b'@00RE004601000155*\\r'), bytearray(b'@00RE004602000156*\\r')]

--- serial_test.py
+++ serial_test.py
@@ -2,8 +2,7 @@ import serial
 from time import sleep
 from functools import reduce
 from operator import xor
-
-
+import json
 # ser = serial.Serial('COM7', baudrate=9600, bytesize=7,
 #                     parity=serial.PARITY_EVEN, stopbits=2, timeout=0.5)
 # cmd = bytearray(b'@00RE004501000156*\x0D')
@@ -14,16 +13,34 @@ from operator import xor
 # ser.close()
 
 
+def ser_obj():
+    return serial.Serial('COM7', baudrate=9600, bytesize=7,
+                         parity=serial.PARITY_EVEN, stopbits=2, timeout=0.5)
+
+
 def calculate_checksum(cmd: str) -> str:
-    # b'@00RE004501000156*\\r'
+    """[create command string--> b'@00RE004501000156*\\r']
+
+    [convert string input to list and integer elements.
+    Perform XOR/sum on elements in the list and convert result to hex.
+    Hex string is then added back to input string together with *cr.
+    resultant string is then converted to bytes and then bytearray. ]
+
+    Arguments:
+        cmd {str} -- [constructed after reading connfig.json]
+
+    Returns:
+        str -- [bytearray]
+    """
     listy = [ord(i) for i in list(cmd)]
     chksum = hex(reduce(xor, map(int, listy)))[2:]
     complete = ''.join((cmd, chksum, r'*\r'))
     final = bytes(complete, encoding="raw_unicode_escape")
-    # final = bytes(complete, encoding="utf-8")
     return bytearray(final)
 
 
-cmd = '@00RE0045440001'
-lol = calculate_checksum(cmd)
-print(lol)
+with open("config.json", "r") as jsonfile:
+    data = json.load(jsonfile)
+command_list = [calculate_checksum(f"@00RE00{i}0001") for i in data.keys()]
+print(command_list)
+# print(data[i]['type'])
